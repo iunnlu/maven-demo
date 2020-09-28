52d70052a0bcc321f034afde3c36c25f331a0e7b
ILHAN UNLU <TCIUNLU@turkcell.entp.tgc>
2020-09-28 18:38:45 +0300

    bolme islemi ile hata alinmasi saglandi

--- src/main/java/com/turkcell/Main.java
+++ src/main/java/com/turkcell/Main.java
@@ -2,6 +2,6 @@ package com.turkcell;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("---------- Selam Arkadaşlar ---------------");
+        System.out.println(9/0);
     }
 }
